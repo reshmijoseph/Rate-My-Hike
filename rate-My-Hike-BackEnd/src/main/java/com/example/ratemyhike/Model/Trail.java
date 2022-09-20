9eb26bdae032df6c8a5c414533443d10c8fdb288
oscar.santana <oscar.santana@cgi.com>
2022-09-20 16:07:53 -0500

    Trail entities created

--- rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Model/Trail.java
+++ rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Model/Trail.java
@@ -1,4 +1,20 @@
 package com.example.ratemyhike.Model;
 
+import org.springframework.data.annotation.Id;
+
 public class Trail {
+    @Id
+    private long id;
+
+    private String trailName;
+
+    private int miles;
+
+    private int elevation;
+
+    private int rating;
+
+    private String level;
+
+
 }
