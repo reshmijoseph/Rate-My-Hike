2e12ade1df338b6ad635f17352efc39fb0285946
Quentin Hewitt <qhewitt@ymail.com>
2022-09-21 09:56:35 -0500

    created methods for Service/ServiceImpl

--- rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Controller/AdminController.java
+++ rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Controller/AdminController.java
@@ -1,12 +1,33 @@
 package com.example.ratemyhike.Controller;
 
+import com.example.ratemyhike.Model.Admin;
+import com.example.ratemyhike.Service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/admin")
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @GetMapping(value = "/admin")
+    public ResponseEntity<List<Admin>> getAllAdminHandler(){
+
+        ResponseEntity<List<Admin>> responseEntity;
+        List<Admin> admins = adminService.getAllAdmins();
+        responseEntity = new ResponseEntity<>(admins, HttpStatus.OK);
+        return responseEntity;
+    }
+
 
 }
--- rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Service/AdminService.java
+++ rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Service/AdminService.java
@@ -1,4 +1,12 @@
 package com.example.ratemyhike.Service;
 
+import com.example.ratemyhike.Model.Admin;
+
+import java.util.List;
+
 public interface AdminService {
+
+    List<Admin> getAllAdmins();
+
+
 }
