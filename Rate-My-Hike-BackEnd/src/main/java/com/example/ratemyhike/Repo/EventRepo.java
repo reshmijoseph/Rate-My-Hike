3896351576bd891b60d95cc6f5615bf65d90b2ae
Dreighck <88525698+Dreighck@users.noreply.github.com>
2022-09-21 13:12:01 -0500

    minor tweaks

--- Rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Repo/EventRepo.java
+++ Rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Repo/EventRepo.java
@@ -3,11 +3,13 @@ package com.example.ratemyhike.Repo;
 import com.example.ratemyhike.Model.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
+@Transactional
 public interface EventRepo extends MongoRepository<Event, AtomicLong> {
 
     Optional<Event> findEventByEventNumber(AtomicLong eventNumber);
--- rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/RatemyhikeApplication.java
+++ rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/RatemyhikeApplication.java
@@ -8,6 +8,7 @@ public class RatemyhikeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RatemyhikeApplication.class, args);
+
     }
 
 }
