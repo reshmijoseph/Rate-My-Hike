8ed71f7d5ebd1db9278b752081e8ea46c7c992aa
reshmi.joseph <reshmi.joseph@cgi.com>
2022-09-23 12:04:48 -0500

    navbar component created

--- Rate-My-Hike-FrontEnd/src/app/app.module.ts
+++ Rate-My-Hike-FrontEnd/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WishlistComponent } from './wishlist/wishlist.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WishlistComponent
-  ],
+    NavbarComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
