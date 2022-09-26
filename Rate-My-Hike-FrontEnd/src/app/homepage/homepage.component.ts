ad8f73e9e32324b27283228f3245fc1b5d23ead7
Kristen Wilson <wilson.kristen614@gmail.com>
2022-09-26 10:18:17 -0500

    testing code

--- Rate-My-Hike-FrontEnd/src/app/homepage/homepage.component.ts
+++ Rate-My-Hike-FrontEnd/src/app/homepage/homepage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Loader } from '@googlemaps/js-api-loader';
-import { ElementRef } from '@angular/core';
-import { Map } from '@angular/google-maps';
+// import { Loader } from '@googlemaps/js-api-loader';
+// import { ElementRef } from '@angular/core';
+// import { ap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-homepage',
@@ -9,27 +9,21 @@ import { Map } from '@angular/google-maps';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  // user:User = new User();
+
+  // map:Map = new Map();
   // errorMessage = "";
 
-  // constructor(private authService:AuthService, private routingService:RoutingService) { }
-
-  // ngOnInit(): void {
-  // }
-  map:Map = new Map();
-  errorMessage = "";
-
-  constructor()
+  
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
  
   
-  const center: google.maps.LatLngLiteral = {lat: 30, lng: -110};
+  // const center: google.maps.LatLngLiteral = {lat: 30, lng: -110};
   
-  function initMap(): void {
-    map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-      center,
-      zoom: 8
-    });
+  // function initMap(): void {
+  //   Map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  //     center,
+  //     zoom: 8
+  //   });
   }
